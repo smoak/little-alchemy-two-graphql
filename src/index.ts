3f695b7e776abca3d63923edde994e3b26762129
Sasha Moak <sasha.moak@gmail.com>
2021-01-21 10:56:42 -0800

    feat(cors): adding in CORS library
    Allowing CORS only from little-alchemy-two-web.netlify.app for now

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors, { CorsOptions } from 'cors';
 import express from 'express';
 import GraphQLHTTP from 'express-graphql';
 
@@ -6,9 +7,13 @@ import schema from './graphql/schema';
 const app = express();
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 const hostname = '0.0.0.0';
+const corsOptions: CorsOptions = {
+  origin: 'https://little-alchemy-two-web.netlify.app',
+};
 
 app.use(
   '/graphql',
+  cors(corsOptions),
   GraphQLHTTP({
     schema,
     graphiql: true,
